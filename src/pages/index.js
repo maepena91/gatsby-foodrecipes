48207b04d5cbc53bdd838377efb991dc688b15e5
Mae Pena <maepena@Maes-iMac.local>
2022-09-11 11:07:43 -0700

    added links to files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Hello People</h1>
+    <div>
+    <Link to="/about">about</Link>
+    </div>
+    <a href="/about">reg Link</a>
+
+
+  </div>
 }
